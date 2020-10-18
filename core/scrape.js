da92ce0d95458d945ada041f7db3e7081b1f9e1b
Axmin <>
2020-10-18 13:57:34 +0545

    Add scrape live market function

---
+++ core/scrape.js
@@ -0,0 +1,28 @@
+const Xray = require('x-ray')
+const { NepalStock } = require('./nepalstock')
+const x = Xray({
+    filters: {
+        trim(value) {
+            return typeof value === 'string' ? value.trim() : value
+        },
+        float(value) {
+            return parseFloat(value)
+        }
+    }
+})
+const fetchMarket = (market = NepalStock.market) => x(market,
+    '#home-contents table.table.table-condensed > tbody> tr',
+    // S.N.	Symbol	LTP	
+    // LTV	Point Change	%Change	Open	High	Low	Volume	Previous Closing
+    [{
+        SN: 'tr>td|trim',
+        CODE: 'tr>td+td|trim',
+        LTP: 'tr>td+td+td|trim|float',
+    }]
+)
+
+const fetchCompanies = () => {
+
+}
+
+module.exports = { fetchMarket, fetchCompanies }
