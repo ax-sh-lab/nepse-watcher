9aee903213ab4311bab7842cad11eb7b3c15f4a6
Axmin <>
2020-10-18 15:19:00 +0545

    Update scrape.test.js

--- core/scrape.test.js
+++ core/scrape.test.js
@@ -3,3 +3,19 @@ const scrape = require('./scrape')
 test('sanity check', () => {
     expect(false).toEqual(!true)
 })
+
+
+describe('live_market.html parse', () => {
+    const fs = require('fs')
+    const rawHtml = fs.readFileSync('./__mocks__/market_live.html', 'utf-8')
+    const market = rawHtml
+    test('check if mock modified', async () => {
+        const data = await scrape.fetchMarket(market)
+        expect(data.length).toEqual(186)
+    })
+
+    test('mock parse', async () => {
+        const data = await scrape.fetchMarket(market)
+        data.forEach(i => global.console.info(i))
+    })
+})
